251869947846b03e950bb3937b06e9d45d9e200b
Juliano Meinen <julianomeinen.souza@gmail.com>
2023-02-23 11:50:44 -0700

    Update example.php

--- src/example.php
+++ src/example.php
@@ -3,6 +3,7 @@
 require_once '../vendor/autoload.php';
 
 use Juliano\Yii2EncryptorBehavior\classes\EncryptDecrypt;
+use Juliano\Yii2EncryptorBehavior\classes\AWSS3;
 
 $key = 'key';
 $text = 'text';
@@ -12,3 +13,15 @@ $decrypted = (new EncryptDecrypt)->decrypt($encrypted, $key);
 echo $text . PHP_EOL;
 echo $encrypted . PHP_EOL;
 echo $decrypted . PHP_EOL;
+
+// Set AWS S3 bucket at instance creation.
+// You can also set the parameter 's3Bucket' in app's config.
+$s3 = new AWSS3('s3-bucket-name');
+        
+// Download decrypted
+$key = 'test-upload-encrypted';
+$destination = 'uploads/test-download-decrypted.txt';
+$s3->downloadDecrypted($key, $destination);
+
+// Upload encrypted
+$s3->uploadEncrypted('php7-text-test-upload-encrypted', 'uploads/php7-text-test-upload.txt');
